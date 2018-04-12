287b0e26934ed3495160b6e729e11bfb72257176
KignorC <kignorchan@gmail.com>
2018-04-12 17:57:57 -0400

    add tester for urlUtil.js, test special urls.

---
+++ test/unit/lib/urlutilTestSpURL.js
@@ -0,0 +1,22 @@
+/* global describe, it */
+const urlUtil = require('../../../js/lib/urlutil')
+const assert = require('assert')
+
+require('../braveUnit')
+
+describe('urlutil', function () {
+  describe('isNotUrl', function () {
+    it('Has space inside url', function () {
+      assert.equal(urlUtil.isNotURL('https://www.google.ca/search?q=dog cat'), true)
+    })
+    it('Has space inside URL and head and back', function () {
+      assert.equal(urlUtil.isNotURL(' https://www.google.ca/search?q=dog cat '), true)
+    })
+    it('File path with space inside', function () {
+      assert.equal(urlUtil.isNotURL('/Users/kignorc/brave-browser/browser-laptop/dog cat.txt'), true)
+    })
+    it('file path with space at head and back and inside', function () {
+      assert.equal(urlUtil.isNotURL(' /Users/kignorc/brave-browser/browser-laptop/dog cat.txt '), true)
+    })
+  })
+})
