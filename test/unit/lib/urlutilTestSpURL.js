70e86f5ebf3fabb5f4a279f35cc4a1c1e05948ac
KignorC <kignorchan@gmail.com>
2018-04-12 22:02:43 -0400

    urlUtilTestSpURL.js update

--- test/unit/lib/urlutilTestSpURL.js
+++ test/unit/lib/urlutilTestSpURL.js
@@ -7,16 +7,16 @@ require('../braveUnit')
 describe('urlutil', function () {
   describe('isNotUrl', function () {
     it('Has space inside url', function () {
-      assert.equal(urlUtil.isNotURL('https://www.google.ca/search?q=dog cat'), true)
+      assert.equal(urlUtil.isURL('https://www.google.ca/search?q=dog cat/'), true)
     })
     it('Has space inside URL and head and back', function () {
-      assert.equal(urlUtil.isNotURL(' https://www.google.ca/search?q=dog cat '), true)
+      assert.equal(urlUtil.isURL(' https://www.google.ca/search?q=dog cat '), true)
     })
     it('File path with space inside', function () {
-      assert.equal(urlUtil.isNotURL('/Users/kignorc/brave-browser/browser-laptop/dog cat.txt'), true)
+      assert.equal(urlUtil.isURL('/Users/kignorc/brave-browser/browser-laptop/dog cat.txt'), true)
     })
     it('file path with space at head and back and inside', function () {
-      assert.equal(urlUtil.isNotURL(' /Users/kignorc/brave-browser/browser-laptop/dog cat.txt '), true)
+      assert.equal(urlUtil.isURL(' /Users/kignorc/brave-browser/browser-laptop/dog cat.txt '), true)
     })
   })
 })
